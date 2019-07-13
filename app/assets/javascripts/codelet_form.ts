0b754396d2604aa30072926d399dc8faa5e92c6b
Michael <michaelc962@yahoo.com>
2019-07-12 22:54:00 -0400

    Add support for outputting code snippets

--- app/assets/javascripts/codelet_form.ts
+++ app/assets/javascripts/codelet_form.ts
@@ -3,7 +3,7 @@ window.addEventListener('load', () => {
     const forms = document.getElementsByClassName('needs-validation');
 
     const validation = Array.prototype.filter.call(forms, (form) => {
-        form.addEventListener('submit', (event) => {
+        form.addEventListener('submit', (event: Event) => {
             if (form.checkValidity() === false) {
                 event.preventDefault();
                 event.stopPropagation();
