c06cfd191bd87640f1691b33fd70636d80b33690
Michael <michaelc962@yahoo.com>
2019-07-17 19:40:53 -0400

    Add markdown support

--- app/models/example.rb
+++ app/models/example.rb
@@ -14,6 +14,9 @@
 class Example < ApplicationRecord
     belongs_to :codelet
 
+    attr_accessor :markdown_command,
+                  :markdown_output
+
     validates :command, presence: true
     validates_length_of :command, maximum: 500
     validates_length_of :output, maximum: 500
@@ -21,5 +24,21 @@ class Example < ApplicationRecord
 
     def publicly_accessible?
         codelet.publicly_accessible?
+    end
+
+    def markdown_command_html
+        if self.markdown_command 
+            self.markdown_command
+        else
+            self.markdown_command = MARKDOWN_RENDERER.render(self.command)
+        end
+    end
+
+    def markdown_output_html
+        if self.markdown_output
+            self.markdown_output
+        else
+            self.markdown_output = MARKDOWN_RENDERER.render(self.output)
+        end
     end
 end
